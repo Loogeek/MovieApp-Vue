5560bcfd07730cc88eb901090505e14214215cb5
Loogeek <loogeek@163.com>
2016-05-25 10:46:10 +0800

    Change Webpack publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist/'),
-    publicPath: '/dist/',
+    publicPath: '/MovieApp-Vue/dist/',
     filename: 'build.js'
   },
   resolveLoader: {
